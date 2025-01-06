d13bbd5c1b51a496f3c03699f7770961c53d7ed5
rafia islam <rafiaivnsc@gmail.com>
2025-01-06 10:02:29 +0600

    upfdate

--- submit-login-admin.php
+++ submit-login-admin.php
@@ -8,10 +8,10 @@ if(isset($_POST['adminID']) && isset($_POST['password'])) {
     $sql="SELECT * FROM admin where ID='$u' AND password='$p'";
     $result =mysqli_query($conn,$sql);
     if (mysqli_num_rows($result)!=0) {
-         // Get the ID (the primary key)
+        
          $row = mysqli_fetch_assoc($result);
          $ID = $row['ID']; 
-         // Store userID in session
+         
          $_SESSION['ID'] = $ID;
 
         header(header:"location:home-admin.php");
